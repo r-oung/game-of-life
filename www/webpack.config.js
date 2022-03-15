52601ec16cd108f3c731e42a02d737dea5eb4b13
Raymond Oung <r.oung@hapi-robo.com>
2022-03-15 16:01:41 +0900

    Changes from `dist/` to `docs/`

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: "./src/bootstrap.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "bundle.js",
   },
   mode: "development",
